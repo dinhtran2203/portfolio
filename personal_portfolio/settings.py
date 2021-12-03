9906551d157905d4d06ac7a10700499d8a75f271
DinhTran <dinh2203@gmail.com>
2021-12-03 16:38:29 +0700

    enviroment key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,21 +11,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # E:\ECE-21-5\Presonal_project\Django\django-portfolio
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3&0)5eb@t+@n8l9g0)o9!q(&#*i1ggdo5ijl2g10ho84z_@y3k'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'mydjangoporfolio.herokuapp.com',
+]
 
 
 # Application definition
